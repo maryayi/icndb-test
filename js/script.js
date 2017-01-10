652357d0398cdb715c80c4d0ad09cfc389f6e4f3
Mahdi Aryayi <mahdiaryayi@gmail.com>
2017-01-10 13:03:37 +0330

    first working version!

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
-$.icndb.getRandomJoke({
-    success: function(joke) {
-        $(".joke").html(joke.joke);
+$.icndb.getCategories(function(cat) {
+    for (var ii = 0; ii < cat.length; ii++) {
+        $(".category").append('<option value="' + cat[ii] + '">' + cat[ii] + '</option>');
     }
 })
+
+$('.button').on('click', function() {
+  $.icndb.getRandomJoke({
+      limitTo: $('.category').val(),
+      success: function(joke) {
+          $(".joke").html(joke.joke);
+      }
+  });
+});
